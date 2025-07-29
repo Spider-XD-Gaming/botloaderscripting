2fcacaa8ebd3f740e1fd0b4d17b781d4eccd674f
Spider <107892868+Spider-XD-Gaming@users.noreply.github.com>
2025-07-29 19:02:41 -0400

    Update render.js

--- render.js
+++ render.js
@@ -12,13 +12,9 @@ const puppeteer = require("puppeteer");
 
   // Wait for cytoscape to finish rendering
   await page.waitForSelector("#cy");
-  await page.waitForFunction(() => {
-    const cy = window.cy;
-    return cy && cy.nodes && cy.nodes().length > 0;
-  });
 
   // Optional: delay just to be safe
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(2000);
 
   const element = await page.$("#cy");
   await element.screenshot({ path: "docs/tree.png" });
